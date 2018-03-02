d4700ad5fa9db5044a199e8dd6ad88e67de68167
Your Name <you@example.com>
2018-03-02 12:56:33 +0100

    added last unittest setup

--- UnitTest/Tests/AllTests.cpp
+++ UnitTest/Tests/AllTests.cpp
@@ -1,7 +1,7 @@
 #include "gtest/gtest.h"
-#include "dummyclassTests.cpp"
+#include "../Source/DummyClass.cpp"
 
 int main(int argc, int **argv) {
 	::testing::InitGoogleTests::
-
+	 return RUN_ALL_TESTS();
 }
--- UnitTest/Tests/DummyClassTest.cpp
+++ UnitTest/Tests/DummyClassTest.cpp
@@ -1,6 +1,12 @@
 #include "gtest/gtest.h"
-#include "../Header/dummyclass.h"
+#include "../Header/DummyClass.h"
 
 namespace {
-	class dummyTest : public dummy
+	class DummyTest : public ::testing::Test {
+	protected:
+		DummyClass _uut;
+	};
+	TEST_F(DummyTest, TestingDummyReturningThree) {
+		ASSERT_EQ(3, _uut.ReturnThree());
+	}
 }
