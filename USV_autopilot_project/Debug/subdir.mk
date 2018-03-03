1c5c93c30592144da498c3d673c8f8b49e46aefa
Your Name <you@example.com>
2018-03-04 00:41:24 +0100

    testing with .ignore

--- USV_autopilot_project/Debug/subdir.mk
+++ USV_autopilot_project/Debug/subdir.mk
@@ -4,13 +4,16 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../main.cpp 
+../main.cpp \
+../test.cpp 
 
 OBJS += \
-./main.o 
+./main.o \
+./test.o 
 
 CPP_DEPS += \
-./main.d 
+./main.d \
+./test.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
