25d003226dd698e12a283e3e34a01432f4bb9a4d
Your Name <you@example.com>
2018-03-04 21:08:29 +0100

    added dummy objects for testing purpose

---
+++ USV_autopilot_project/Header/AnotherDummy.h
@@ -0,0 +1,12 @@
+#ifndef _ANOTHERDUMMY_H
+#define _ANOTHERDUMMY_H
+
+class AnotherDummy {
+public:
+	AnotherDummy();
+	int ReturnThree();
+	void setRPM(double percentage);
+
+};
+
+#endif
---
+++ USV_autopilot_project/Source/AnotherDummy.cpp
@@ -0,0 +1,11 @@
+#include "../Header/AnotherDummy.h"
+AnotherDummy::AnotherDummy() {
+
+}
+
+int AnotherDummy::ReturnThree() {
+	return 3;
+}
+
+void AnotherDummy::setRPM(double percentage) {
+}
