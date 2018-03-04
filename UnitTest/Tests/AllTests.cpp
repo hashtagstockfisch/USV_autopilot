4de2eec85fd49bcb89793fc3355b9db85746dce7
Your Name <you@example.com>
2018-03-04 21:27:03 +0100

    added testing stuff

--- UnitTest/Tests/AllTests.cpp
+++
@@ -1,8 +0,0 @@
-#include "gtest/gtest.h"
-#include "/UnitTests/DummyClassTest.cpp"
-
-int main(int argc, char **argv) {
-	::testing::InitGoogleTest(&argc, argv);
-	//hello
-	 return RUN_ALL_TESTS();
-}
