bbeeca15da5480be01f8c0e05781edc54d6ddf66
Your Name <you@example.com>
2018-03-05 14:37:18 +0100

    added small change to config file

--- USV_autopilot_project/Header/Config.h
+++ USV_autopilot_project/Header/Config.h
@@ -1,21 +1,14 @@
 #ifndef _CONFIG_H
 #define _CONFIG_H
 #include "USVMotorProtocol.h"
-#include "../Interfaces/AllInterfaces.h"
 #include <iostream>
 #include <string.h>
 
 using namespace std;
-
-void InitConfig();
 struct ConfigParameters{
 	string _motorTransportLayer;
 	string _motorDataLinklayer;
-	string _GNSSDriver;
-	string _GNSSTransportlayer;
 	string _GNSSDataLinklayer;
 };
-
-
 
 #endif
